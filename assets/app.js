2592ff7bffaf52123eb48c78862d0f7da19dc834
Bess1k <bmehmeti@gmail.com>
2026-04-01 10:46:08 +0200

    Installation Bootstrap

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+import './stimulus_bootstrap.js';
+
+/* Import des fichiers de Bootstrap */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+
+/* Import des styles CSS de l'application */
+import './styles/app.css';
