1b291f8e6c9512d8ab928a47fd74fe2497a08166
Bess1k <bmehmeti@gmail.com>
2026-04-10 10:53:08 +0200

    Ajout Dashboard avec cartes coffre, bénéficiaires et mode vacances

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+final class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        // Récupérer l'utilisateur connecté
+        $user = $this->getUser();
+
+        return $this->render('dashboard/index.html.twig', [
+            'user' => $user,
+            'vaultElements' => $user->getVaultElements(),
+            'beneficiaries' => $user->getBeneficiaries(),
+        ]);
+    }
+}
