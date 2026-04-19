b2d2018852ccba8d5ec408c2ff30b3d169394adb
Bess1k <bmehmeti@gmail.com>
2026-04-19 15:12:07 +0200

    Renforcement sécurité inscription + changer default locate

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -9,18 +9,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\PasswordStrength;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        // Construction du formulaire d'inscription
-        // Chaque ->add() ajoute un champ au formulaire
         $builder
             ->add('lastname', TextType::class, [
                 'label' => 'Nom'
@@ -28,51 +29,48 @@ class RegistrationFormType extends AbstractType
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom'
             ])
-            // Champ date de naissance : widget 'single_text' génère un input type="date" HTML5
+            // Date de naissance : widget 'single_text' génère un input type="date" HTML5
             ->add('birthDate', DateType::class, [
                 'label' => 'Date de naissance',
                 'widget' => 'single_text',
                 'required' => true,
             ])
-
             ->add('birthPlace', TextType::class, [
                 'label' => 'Lieu de naissance',
             ])
-            
             ->add('email', EmailType::class, [
                 'label' => 'Adresse e-mail de connexion'
             ])
-
-
-            // Case à cocher "J'accepte les conditions" 
-            // 'mapped' => false : ce champ n'existe pas dans l'entité User,
-            // il sert uniquement à la validation du formulaire
+            // Case à cocher "J'accepte les conditions"
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => "Accepter les conditions d'utilisation",
                 'constraints' => [
                     new IsTrue(
                         message: 'Vous devez accepter nos conditions.',
                     ),
                 ],
             ])
-
-            // Champ mot de passe en clair
-            // 'mapped' => false : le mot de passe n'est pas stocké directement dans User,
-            // il sera hashé par le controller avant d'être enregistré dans la base
-            ->add('plainPassword', PasswordType::class, [
+            // Mot de passe avec confirmation 
+            ->add('plainPassword', RepeatedType::class, [
                 'mapped' => false,
-                'label'         => "Mot de passe",
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les champs mot de passe doivent être identiques',
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    // Le mot de passe ne peut pas être vide
                     new NotBlank(
                         message: 'Veuillez entrer un mot de passe',
                     ),
-                    // Longueur minimum 6 caractères, maximum 4096 (limite Symfony pour sécurité)
                     new Length(
-                        min: 6,
+                        min: 12,
                         minMessage: 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         max: 4096,
                     ),
+                    new PasswordStrength(),
+                    new NotCompromisedPassword(),
                 ],
             ])
         ;
